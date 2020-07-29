77e18321a126a7a1831397fe3df0d4e2f258c138
k-abhi1512 <2akj1018@gmail.com>
2020-07-30 02:50:51 +0530

    completed calculator app

--- webapplication/calculator/urls.py
+++ webapplication/calculator/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name="homepage")
+    path('',views.home, name="homepage"),
+    path('news',views.news, name="newspage")
 
 ]
--- webapplication/calculator/views.py
+++ webapplication/calculator/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello Welcome to Django World!")
+    return render(request,'index.html',{'name':'The Busy Digi'})
+
+def news(request):
+
+    var1 = int(request.POST['num1'])
+    var2 = int(request.POST['num2'])
+    result = var1 * var2
+    return render(request,'news.html',{'result': result})
