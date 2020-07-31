16913c17e40150d86ed7bf87f98f1ad3e9a5dbaa
k-abhi1512 <2akj1018@gmail.com>
2020-07-31 22:46:38 +0530

    Created Dynamic page and connected with postgresql database

--- webapplication/travello/models.py
+++ webapplication/travello/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Destinations(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='pics/')
+    descr = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
--- webapplication/travello/views.py
+++ webapplication/travello/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index2.html')
+    dest = Destinations.objects.all()
+    return render(request, 'index2.html',{'dest':dest})
